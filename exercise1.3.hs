d760e10a4fa58d9b5c7d78291177c76d3a72c86c
CodyReichert <cody@reichertbrothers.com>
2014-05-16 17:37:40 -0500

    updated formatting

--- exercise1.3.hs
+++ exercise1.3.hs
@@ -1,7 +1,6 @@
 squared x = x * x
-
 bigger x y = if x > y then x else y
 
 sumOfBigger x y z = if x > y
-					then squared x + squared (bigger y z)
-					else squared y + squared (bigger x z)
+                        then squared x + (bigger z y)
+                        else squared y + (bigger z x)
