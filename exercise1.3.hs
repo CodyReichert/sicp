f09b1bd4f2d0bba2131495cdc108bcb52ed901ea
Cody Reichert' <cody@reichertbrothers.com>
2014-05-16 21:41:36 -0500

    Delete exercise1.3.hs

--- exercise1.3.hs
+++
@@ -1,6 +0,0 @@
-squared x = x * x
-bigger x y = if x > y then x else y
-
-sumOfBigger x y z = if x > y
-                        then squared x + (bigger z y)
-                        else squared y + (bigger z x)
